68bb32b79d9e7dea415fbad42880e2d0669581e7
yyoshikai <yyoshikai12@gmail.com>
2024-09-28 17:07:02 +0900

    tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -10,17 +10,17 @@ class MoleculeProteinTokenizer:
         self.mol_start_token = 2
         self.prot_start_token = 3
 
-        self.coord_min = -20
-        self.coord_max = 20
         self.coord_i_offset = 4
-        self.coord_f_offset = 1004
+        self.coord_f_offset = self.coord_i_offset + (COORD_SUP - COORD_MIN)
         
 
         pass
     
     def tokenize_coord(self, coord: np.ndarray) -> list[int]:
-        coord = np.clip(coord, COORD_MIN, COORD_SUP-0.001)-COORD_MIN
-        coord_i = coord.astype(int)
+        coord = np.clip(coord, COORD_MIN, COORD_SUP-0.001) - COORD_MIN
+        coord_i = coord.astype(int) + self.coord_i_offset
+        coord_f = ((coord % 1)*1000).astype(int) + self.coord_f_offset
+        tokens = np.stack([coord])
 
     def tokenize_mol(self) -> list[int]:
         pass
