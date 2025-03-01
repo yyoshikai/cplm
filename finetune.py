42d6f4eb0f1c71ed3ebd5c6fb1f31a2a86e11a3b
yyoshikai <yyoshikai12@gmail.com>
2025-03-01 11:53:31 +0900

    merged train

--- finetune.py
+++ finetune.py
@@ -1,31 +1,23 @@
 # 241025 若干argumentを変更した。
 import sys, os
 import argparse, logging
-import math, gc
 from glob import glob
 
-import psutil, yaml
+import yaml
 from addict import Dict
-import numpy as np
-import pandas as pd
 import torch
-import torch.nn as nn
 import torch.distributed as dist
-from torch.utils.data.dataset import Subset
+from torch.utils.data import Subset
 from torch.nn.parallel import DistributedDataParallel
-import torch.optim.lr_scheduler as lrs
 WORKDIR = os.environ.get('WORKDIR', "/workspace")
 sys.path.append(WORKDIR)
 
-from src.data import LMDBDataset, FinetuneDataset, CDDataset, DDPStringCollateLoader
-
-from src.data.tokenizer import StringTokenizer, FloatTokenizer, ProteinAtomTokenizer,\
-    VocEncoder, TokenEncodeDataset
+from src.data import *
+from src.data.tokenizer import TokenEncodeDataset, VocEncoder
 from src.model import Model
-from src.utils import RandomState, set_logtime, rectime
-from src.utils.path import timestamp, cleardir
-from src.utils.logger import add_file_handler, add_stream_handler, INFO_WORKER
-from src.utils.rdkit import set_rdkit_logger
+from src.utils import set_logtime
+from src.utils.path import timestamp
+from src.utils.train import WeightedCELoss, train, add_train_args, get_train_logger, MAIN_RANK
 
 # arguments
 parser = argparse.ArgumentParser()
@@ -38,15 +30,11 @@ parser.add_argument("--pretrain-name", required=True)
 parser.add_argument("--pretrain-step", type=int)
 
 ## hyperparameters
-parser.add_argument("--token-per-step", type=int, default=int(1.6e6))
-parser.add_argument("--max-step", type=int, default=1000000)
-parser.add_argument("--max-opt-step", type=int, default=float('inf'))
-parser.add_argument("--weight-decay", type=float, default=0.01)
-parser.add_argument("--clip-grad-value", type=float, default=None)
-parser.add_argument("--clip-grad-norm", type=float, default=1.0)
-parser.add_argument("--coord-noise-std", type=float, default=50.0)
+add_train_args(parser)
+
+
+
 parser.add_argument("--coord-range", type=float, help='Defaults to value in training')
-parser.add_argument("--loss-scale")
 parser.add_argument("--pocket-coord-heavy", action='store_true')
 parser.add_argument("--reset-nan-grad", action='store_true')
 
@@ -58,29 +46,21 @@ parser.add_argument("--scheduler", default='warmup', choices=['warmup', 'step'])
 parser.add_argument("--finetune-save-dir", required=True)
 parser.add_argument("--index-lmdb")
 
-## environments
-parser.add_argument("--token-per-batch", type=int, default=25000)
-parser.add_argument("--record-opt-step", type=int)
-parser.add_argument("--seed", type=int, default=0)
-parser.add_argument("--num-workers", type=int, default=0)
-parser.add_argument("--pin-memory", action='store_true')
-parser.add_argument("--prefetch-factor", type=int)
-parser.add_argument("--sdp-kernel", choices=['FLASH', 'CUDNN', 'MATH', 'EFFICIENT'])
-parser.add_argument("--gc", action='store_true')
-parser.add_argument("--logtime", action='store_true')
-parser.add_argument("--tokenizer-log-interval", type=int)
-parser.add_argument("--duplicate", default='ask')
 
 args = parser.parse_args()
 
 # environment
 if args.test: args.studyname+='_test'
-
-pretrain_dir = f"training/results/{args.pretrain_name}"
 if args.record_opt_step is None:
     args.record_opt_step = 1 if args.test else 1000
 if args.tokenizer_log_interval is None:
     args.tokenizer_log_interval = 10000 if args.test else int(1e7)
+
+# load pretrain
+pretrain_dir = f"training/results/{args.pretrain_name}"
+pretrain_config = Dict(yaml.safe_load(open(f"{pretrain_dir}/config.yaml")))
+if args.coord_range is None:
+    args.coord_range = pretrain_config.coord_range
 
 auto_pretrain_step = False
 if args.pretrain_step is None:
@@ -90,7 +70,7 @@ if args.pretrain_step is None:
     args.pretrain_step = max(steps)
     auto_pretrain_step = True
 
-main_rank = 0
+
 batch_first = False
 set_logtime(args.logtime)
 
@@ -100,52 +80,13 @@ rank = dist.get_rank()
 size = dist.get_world_size()
 device = torch.device('cuda', index=rank % torch.cuda.device_count()) \
     if torch.cuda.is_available() else torch.device('cpu')
-is_main = rank == main_rank
+is_main = rank == MAIN_RANK
 
 ## make result dir
 result_dir = f"finetune/results/{timestamp()}_{args.studyname}"
-if is_main:
-    cleardir(result_dir)
-    os.makedirs(f"{result_dir}/models", exist_ok=True)
-    os.makedirs(f"{result_dir}/step_data", exist_ok=True)
-    os.makedirs(f"{result_dir}/optimizers", exist_ok=True)
-dist.barrier()
-
-## load config
-pretrain_config = Dict(yaml.safe_load(open(f"{pretrain_dir}/config.yaml")))
-if args.coord_range is None:
-    args.coord_range = pretrain_config.coord_range
-
-## save args
-if is_main:
-    with open(f"{result_dir}/config.yaml", 'w') as f:
-        yaml.dump(vars(args), f)
-
-## seed
-rstate = RandomState(args.seed)
-if args.test:
-    torch.backends.cudnn.deterministic = True
-    torch.use_deterministic_algorithms(True, warn_only=True)
-    torch.backends.cudnn.benchmark = False
-
-## scaled dot product attention kernel
-if args.sdp_kernel is not None:
-    torch.backends.cuda.enable_flash_sdp(args.sdp_kernel == 'FLASH')
-    torch.backends.cuda.enable_cudnn_sdp(args.sdp_kernel == 'CUDNN')
-    torch.backends.cuda.enable_math_sdp(args.sdp_kernel == 'MATH')
-    torch.backends.cuda.enable_mem_efficient_sdp(args.sdp_kernel == 'EFFICIENT')
 
 ## logger
-fmt = "[{asctime}]"+f"[{rank}/{size}]"+"[{name}][{levelname}]{message}"
-logger = logging.getLogger()
-add_stream_handler(logger, logging.INFO, fmt=fmt)
-add_file_handler(logger, f"{result_dir}/debug.log", logging.DEBUG, fmt=fmt, mode='a')
-add_file_handler(logger, f"{result_dir}/info.log", logging.INFO, fmt=fmt, mode='a')
-logger.setLevel(logging.NOTSET if is_main else INFO_WORKER)
-log_step = 1 if args.test else 1000
-set_rdkit_logger()
-
-### log environment
+logger = get_train_logger(result_dir)
 logger.info(f"num_workers={args.num_workers}")
 if auto_pretrain_step:
     logger.info(f"pretrain_step was set to {args.pretrain_step}")
@@ -165,12 +106,13 @@ train_data = FinetuneDataset(cddata, protein_atom_tokenizer, float_tokenizer, sm
 vocs = train_data.vocs()
 voc_encoder = VocEncoder(vocs)
 train_data = TokenEncodeDataset(train_data, voc_encoder)
-if rank != main_rank:
+if not is_main:
     del train_data
     train_data = None
 
+# Make dataset
 train_loader = DDPStringCollateLoader(train_data, args.num_workers, args.pin_memory, args.prefetch_factor, 
-    args.token_per_batch, batch_first, voc_encoder.pad_token, device, size, rank, main_rank)
+    args.token_per_batch, batch_first, voc_encoder.pad_token, device, size, rank, MAIN_RANK)
 
 # model
 model = Model(8, 768, 12, 4, 0.1, 'gelu', True, voc_encoder.i2voc, voc_encoder.pad_token)
@@ -183,186 +125,8 @@ state_dict = torch.load(f"{pretrain_dir}/models/{args.pretrain_step}.pth",
     map_location=device, weights_only=True)
 model.load_state_dict(state_dict)
 
-## criterion, optimizer
-criterion = nn.CrossEntropyLoss(reduction='none', ignore_index=voc_encoder.pad_token)
-optimizer = torch.optim.AdamW(model.parameters(), weight_decay=args.weight_decay, lr=args.lr)
-optimizer.zero_grad()
-match args.loss_scale:
-    case None:
-        loss_scale = 1
-    case 'token_per_batch':
-        loss_scale = 1/args.token_per_step
-    case _:
-        loss_scale = float(args.loss_scale)
+criterion = WeightedCELoss(voc_encoder, args.seed)
 
-match args.scheduler:
-    case 'warmup':
-        def schedule(step: int):
-            if step <= 2000:
-                return step / 2000
-            elif step <= 55000:
-                return math.cos(math.pi*((step-2000)/(55000-2000)))*0.49+0.51
-            else:
-                return 0.02
-    case 'step':
-        def schedule(step: int):
-            return 0.02 ** (step / 55000)
-    case _:
-        raise ValueError
-scheduler = lrs.LambdaLR(optimizer, schedule)
-    
+train(args, train_loader, model, criterion, result_dir, is_main)
 
-accum_loss = 0
-opt_step = 0
-n_accum_token = 0
-accum_losses = []
-accum_n_tokens = []
-lrs = []
-mems = []
-
-data_times = []
-loss_times = []
-batch_sizes = []
-max_lens = []
-nan_grad_step_saved = False
-
-# save at step 0
-if is_main:
-    torch.save(model.state_dict(), f"{result_dir}/models/{opt_step}.pth")
-    cleardir(f"{result_dir}/optimizers")
-    torch.save(optimizer.state_dict(), f"{result_dir}/optimizers/{opt_step}.pth")
-            
-
-logger.info("Training started.")
-for step in range(args.max_step):
-
-    # get batch
-    with rectime() as data_timer:
-        batch = train_loader.__next__()
-        batch = batch.to(device)
-        n_token = torch.sum(batch != voc_encoder.pad_token).item()
-        n_accum_token += n_token
-
-        batch_sizes.append(batch.shape[1])
-        max_lens.append(batch.shape[0])
-        # make weight
-        weight = torch.zeros_like(batch, dtype=torch.float)
-        smi_count = torch.cumsum(batch == voc_encoder.voc2i['[LIGAND]'], dim=0)
-        weight[smi_count >= 1] = 1
-        coord_count = torch.cumsum(batch == voc_encoder.voc2i['[XYZ]'], dim=0)
-        weight[coord_count >= 2] = 5
-        end_count = torch.cumsum(batch == voc_encoder.voc2i['[END]'], dim=0)
-        weight[end_count >= 1] = 0
-        
-        # log tokens in initial few steps
-        if step < 5:
-            rstate = np.random.RandomState(args.seed+step)
-            idxs = np.arange(batch.shape[1])
-            if len(idxs) > 10: 
-                idxs = np.sort(rstate.choice(batch.shape[1], size=10, replace=False))
-            logger.log(INFO_WORKER, f"batch of step {step}:")
-            for idx in idxs:
-                logger.log(INFO_WORKER, f"  [{idx:3}]={','.join(voc_encoder.decode(batch[:,idx].cpu().tolist()))}")
-                logger.log(INFO_WORKER, f"  weight[{idx:3}]={weight[:,idx].cpu().tolist()}")
-
-    data_times.append(data_timer.time)
-
-    with rectime() as loss_timer:
-        with torch.autocast('cuda', dtype=torch.bfloat16):
-
-            pred = model(batch[:-1])
-            loss = torch.sum(criterion(pred.reshape(-1, voc_encoder.voc_size), batch[1:].ravel())*weight[:-1].ravel()) * loss_scale
-            loss.backward()
-
-            """ gradientの分布を表示
-            grad_max = -torch.inf
-            grad_min = torch.inf
-            grad_norm = 0.0
-            for param in model.parameters():
-                grad = param.grad
-                grad_max = max(grad_max, torch.max(grad))
-                grad_min = min(grad_min, torch.min(grad))
-                grad_norm += torch.sum(grad**2)
-            logger.info(f"grad_max={grad_max.item()}, grad_min={grad_min}, grad_norm={grad_norm.item()}")
-            """
-            
-            # check nan
-            if args.reset_nan_grad:
-                grad_is_finite = np.all([torch.all(torch.isfinite(param.grad)).item() for param in model.parameters()])
-                if not grad_is_finite:
-                    if is_main:
-                        logger.warning("nan or infinite value in gradient. Gradient is reset.")
-                        for name, param in model.module.named_parameters():
-                            n_nan = torch.sum(torch.isnan(param.grad)).item()
-                            n_inf = torch.sum(torch.isinf(param.grad)).item()
-                            if n_nan > 0 or n_inf > 0:
-                                logger.warning(f"{name}: {n_nan=}, {n_inf=}")
-
-                    ## save situation
-                    if is_main and not nan_grad_step_saved:
-                        nan_dir = f"{result_dir}/nan_step_{step}/{rank}"
-                        os.makedirs(nan_dir, exist_ok=True)
-                        torch.save(batch.detach().cpu(), f"{nan_dir}/batch.pt")
-                        torch.save(model.state_dict(), f"{nan_dir}/model.pth")
-                        nan_grad_step_saved = True
-                    
-                    ## reset grad
-                    n_accum_token = 0
-                    accum_loss = 0
-                    optimizer.zero_grad()
-
-        accum_loss += loss.item()
-    loss_times.append(loss_timer.time)
-
-    # sum accum_token
-    reduced_accum_token = torch.tensor(n_accum_token, dtype=torch.int, device=device)
-    dist.all_reduce(reduced_accum_token)
-
-    if reduced_accum_token >= args.token_per_step:
-        with rectime() as optim_timer:
-            if args.clip_grad_value is not None:
-                torch.nn.utils.clip_grad_value_(model.parameters(), args.clip_grad_value)
-            torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad_norm)
-            optimizer.step()
-            optimizer.zero_grad()
-        if args.test:
-            logger.info(f"optim_time={optim_timer.time:.03f}")
-        opt_step += 1
-        accum_losses.append(accum_loss)
-        accum_n_tokens.append(n_accum_token)
-        lrs.append(scheduler.get_last_lr()[0])
-
-        mems.append(psutil.virtual_memory().used/(2**30))
-
-        scheduler.step()
-        if opt_step % args.record_opt_step == 0:
-            df = pd.DataFrame({
-                'loss': accum_losses,
-                'n_token': accum_n_tokens,
-                'lr': lrs,
-                'memory': mems
-            })
-            df.to_csv(f"{result_dir}/step_data/{rank}.csv")
-            df = pd.DataFrame({
-                'data_time': data_times,
-                'forward_time': loss_times,
-                'batch_size': batch_sizes,
-                'max_len': max_lens
-            })
-            df.to_csv(f"{result_dir}/step_data/{rank}_step.csv")
-            if is_main:
-                torch.save(model.state_dict(), f"{result_dir}/models/{opt_step}.pth")
-                cleardir(f"{result_dir}/optimizers")
-                torch.save(optimizer.state_dict(), f"{result_dir}/optimizers/{opt_step}.pth")
-            if args.gc:
-                gc.collect()
-        n_accum_token = 0
-        accum_loss = 0
-
-        if opt_step >= args.max_opt_step:
-            break
-    if (step+1) % log_step == 0:
-        logger.info(f"{step+1} step finished.")
-logger.info("Training finished!")
 dist.destroy_process_group()
-
---
+++ src/utils/train.py
@@ -0,0 +1,312 @@
+import sys, os, math, gc, yaml, psutil, logging
+from argparse import ArgumentParser, Namespace
+from logging import getLogger
+from collections.abc import Iterator
+import numpy as np
+import pandas as pd
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import LambdaLR
+from ..data.tokenizer import VocEncoder
+from ..utils.logger import INFO_WORKER, add_stream_handler, add_file_handler
+from ..utils.rdkit import set_rdkit_logger
+from ..model import Model
+from ..utils.path import cleardir
+from src.utils import RANDOM_STATE, rectime
+
+MAIN_RANK = 0
+
+## criterion, optimizer
+from torch import Tensor
+
+class CELoss(nn.Module):
+    logger = getLogger(f"{__module__}.{__qualname__}")
+    def __init__(self, voc_encoder: VocEncoder, seed: int):
+        super().__init__()
+        self.voc_encoder = voc_encoder
+        self.step = 0
+        self.seed = seed
+
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        """
+        input: Tensor[L-1, B, N]
+        target: Tensor[L-1, B]    
+        
+        """
+        # log tokens in initial few steps
+        if self.step < 5:
+            rstate = np.random.RandomState(self.seed+self.step)
+            idxs = np.arange(target.shape[1])
+            if len(idxs) > 10: 
+                idxs = np.sort(rstate.choice(target.shape[1], size=10, replace=False))
+            self.logger.log(INFO_WORKER, f"target of step {self.step}:")
+            for idx in idxs:
+                self.logger.log(INFO_WORKER, f"  [{idx:3}]={','.join(self.voc_encoder.decode(target[:,idx].cpu().tolist()))}")
+        self.step += 1
+
+        return F.cross_entropy(input.reshape(-1, self.voc_encoder.voc_size), target.ravel(), reduction='sum', ignore_index=self.voc_encoder.pad_token)
+
+class WeightedCELoss(nn.Module):
+    logger = getLogger(f"{__module__}.{__qualname__}")
+    def __init__(self, voc_encoder: VocEncoder, seed: int):
+        super().__init__()
+        self.step = 0
+        self.voc_encoder = voc_encoder
+        self.seed = seed
+
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        """
+        input: Tensor[L-1, B, N]
+        target: Tensor[L-1, B]
+        
+        """
+        
+        # make weight
+        weight = torch.zeros_like(target, dtype=torch.float)
+        smi_count = torch.cumsum(target == self.voc_encoder.voc2i['[LIGAND]'], dim=0)
+        weight[smi_count >= 1] = 1
+        coord_count = torch.cumsum(target == self.voc_encoder.voc2i['[XYZ]'], dim=0)
+        weight[coord_count >= 2] = 5
+        end_count = torch.cumsum(target == self.voc_encoder.voc2i['[END]'], dim=0)
+        weight[end_count >= 1] = 0
+        
+
+        # log tokens in initial few steps
+        if self.step < 5:
+            rstate = np.random.RandomState(self.seed+self.step)
+            idxs = np.arange(target.shape[1])
+            if len(idxs) > 10: 
+                idxs = np.sort(rstate.choice(target.shape[1], size=10, replace=False))
+            self.logger.log(INFO_WORKER, f"target of step {self.step}:")
+            for idx in idxs:
+                self.logger.log(INFO_WORKER, f"  [{idx:3}]={','.join(self.voc_encoder.decode(target[:,idx].cpu().tolist()))}")
+                self.logger.log(INFO_WORKER, f"  weight[{idx:3}]={weight[:,idx].cpu().tolist()}")
+        self.step += 1
+        return torch.sum(F.cross_entropy(input.reshape(-1, self.voc_encoder.voc_size), target.ravel(), reduction='none')*weight)
+
+def set_sdp_kernel(sdp_kernel: str|None):
+    if sdp_kernel is not None:
+        assert sdp_kernel in ['FLASH', 'CUDNN', ]
+        torch.backends.cuda.enable_flash_sdp(sdp_kernel == 'FLASH')
+        torch.backends.cuda.enable_cudnn_sdp(sdp_kernel == 'CUDNN')
+        torch.backends.cuda.enable_math_sdp(sdp_kernel == 'MATH')
+        torch.backends.cuda.enable_mem_efficient_sdp(sdp_kernel == 'EFFICIENT')
+
+def get_train_logger(result_dir):
+    rank = dist.get_rank()
+    size = dist.get_world_size()
+    fmt = "[{asctime}]"+f"[{rank}/{size}]"+"[{name}][{levelname}]{message}"
+    logger = getLogger()
+    add_stream_handler(logger, logging.INFO, fmt=fmt)
+    add_file_handler(logger, f"{result_dir}/debug.log", logging.DEBUG, fmt=fmt, mode='a')
+    add_file_handler(logger, f"{result_dir}/info.log", logging.INFO, fmt=fmt, mode='a')
+    logger.setLevel(logging.NOTSET if rank == MAIN_RANK else INFO_WORKER)
+    set_rdkit_logger()
+    return logger
+
+def add_train_args(parser: ArgumentParser):
+    ### training
+    parser.add_argument("--token-per-step", type=int, default=int(1.6e6))
+    parser.add_argument("--max-step", type=int, default=1000000)
+    parser.add_argument("--max-opt-step", type=int, default=float('inf'))
+    parser.add_argument("--weight-decay", type=float, default=0.01)
+    parser.add_argument("--clip-grad-value", type=float, default=None)
+    parser.add_argument("--clip-grad-norm", type=float, default=1.0)
+    parser.add_argument("--coord-noise-std", type=float, default=50.0)
+    parser.add_argument("--loss-scale")
+
+    ## environments
+    parser.add_argument("--token-per-batch", type=int, default=25000)
+    parser.add_argument("--record-opt-step", type=int)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--num-workers", type=int, default=0)
+    parser.add_argument("--pin-memory", action='store_true')
+    parser.add_argument("--prefetch-factor", type=int)
+    parser.add_argument("--sdp-kernel", choices=['FLASH', 'CUDNN', 'MATH', 'EFFICIENT'])
+    parser.add_argument("--gc", action='store_true')
+    parser.add_argument("--logtime", action='store_true')
+    parser.add_argument("--tokenizer-log-interval", type=int)
+    parser.add_argument("--duplicate", default='ask')
+
+def train(args: Namespace, train_loader: Iterator, model: Model, criterion: nn.Module, result_dir: str, pad_token: int, device: torch.device, log_step):
+    logger = getLogger('train')
+
+
+    # Environment
+
+    ## rank
+    rank = dist.get_rank()
+    is_main = rank == MAIN_RANK
+
+    if is_main:
+        cleardir(result_dir)
+        os.makedirs(f"{result_dir}/models", exist_ok=True)
+        os.makedirs(f"{result_dir}/step_data", exist_ok=True)
+        os.makedirs(f"{result_dir}/optimizers", exist_ok=True)
+    dist.barrier()
+
+    ## save args
+    if is_main:
+        with open(f"{result_dir}/config.yaml", 'w') as f:
+            yaml.dump(vars(args), f)
+
+    ## fix seed
+    RANDOM_STATE.seed(args.seed)
+    if args.test:
+        torch.backends.cudnn.deterministic = True
+        torch.use_deterministic_algorithms(True, warn_only=True)
+        torch.backends.cudnn.benchmark = False
+
+    ## scaled dot product attention kernel
+    set_sdp_kernel(args.sdp_kernel)
+
+    ## optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), weight_decay=args.weight_decay, lr=args.lr)
+    optimizer.zero_grad()
+    match loss_scale:
+        case None:
+            loss_scale = 1
+        case 'token_per_batch':
+            loss_scale = 1/args.token_per_step
+        case _:
+            loss_scale = float(loss_scale)
+
+    ## scheduler
+    match scheduler:
+        case 'warmup':
+            def schedule(step: int):
+                if step <= 2000:
+                    return step / 2000
+                elif step <= 55000:
+                    return math.cos(math.pi*((step-2000)/(55000-2000)))*0.49+0.51
+                else:
+                    return 0.02
+        case 'step':
+            def schedule(step: int):
+                return 0.02 ** (step / 55000)
+        case _:
+            raise ValueError
+    scheduler = LambdaLR(optimizer, schedule)
+
+    accum_loss = 0
+    opt_step = 0
+    n_accum_token = 0
+    accum_losses = []
+    accum_n_tokens = []
+    lrs = []
+    mems = []
+
+    data_times = []
+    loss_times = []
+    batch_sizes = []
+    max_lens = []
+    nan_grad_step_saved = False
+
+    # save at step 0
+    if is_main:
+        torch.save(model.state_dict(), f"{result_dir}/models/{opt_step}.pth")
+        cleardir(f"{result_dir}/optimizers")
+        torch.save(optimizer.state_dict(), f"{result_dir}/optimizers/{opt_step}.pth")
+                
+
+    logger.info("Training started.")
+    for step in range(args.max_step):
+
+        # get batch
+        with rectime() as data_timer:
+            batch = train_loader.__next__()
+            batch = batch.to(device)
+            n_token = torch.sum(batch != pad_token).item()
+            n_accum_token += n_token
+
+            batch_sizes.append(batch.shape[1])
+            max_lens.append(batch.shape[0])
+        data_times.append(data_timer.time)
+
+        with rectime() as loss_timer:
+            with torch.autocast('cuda', dtype=torch.bfloat16):
+                pred = model(batch[:-1])
+                loss = criterion(pred, batch) * loss_scale
+                loss.backward()
+
+                
+                # check nan
+                if args.reset_nan_grad:
+                    grad_is_finite = np.all([torch.all(torch.isfinite(param.grad)).item() for param in model.parameters()])
+                    if not grad_is_finite:
+                        if is_main:
+                            logger.warning("nan or infinite value in gradient. Gradient is reset.")
+                            for name, param in model.module.named_parameters():
+                                n_nan = torch.sum(torch.isnan(param.grad)).item()
+                                n_inf = torch.sum(torch.isinf(param.grad)).item()
+                                if n_nan > 0 or n_inf > 0:
+                                    logger.warning(f"{name}: {n_nan=}, {n_inf=}")
+
+                        ## save situation
+                        if is_main and not nan_grad_step_saved:
+                            nan_dir = f"{result_dir}/nan_step_{step}/{rank}"
+                            os.makedirs(nan_dir, exist_ok=True)
+                            torch.save(batch.detach().cpu(), f"{nan_dir}/batch.pt")
+                            torch.save(model.state_dict(), f"{nan_dir}/model.pth")
+                            nan_grad_step_saved = True
+                        
+                        ## reset grad
+                        n_accum_token = 0
+                        accum_loss = 0
+                        optimizer.zero_grad()
+
+            accum_loss += loss.item()
+        loss_times.append(loss_timer.time)
+
+        # sum accum_token
+        reduced_accum_token = torch.tensor(n_accum_token, dtype=torch.int, device=device)
+        dist.all_reduce(reduced_accum_token)
+
+        if reduced_accum_token >= args.token_per_step:
+            with rectime() as optim_timer:
+                if args.clip_grad_value is not None:
+                    torch.nn.utils.clip_grad_value_(model.parameters(), args.clip_grad_value)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad_norm)
+                optimizer.step()
+                optimizer.zero_grad()
+            if args.test:
+                logger.info(f"optim_time={optim_timer.time:.03f}")
+            opt_step += 1
+            accum_losses.append(accum_loss)
+            accum_n_tokens.append(n_accum_token)
+            lrs.append(scheduler.get_last_lr()[0])
+
+            mems.append(psutil.virtual_memory().used/(2**30))
+
+            scheduler.step()
+            if opt_step % args.record_opt_step == 0:
+                df = pd.DataFrame({
+                    'loss': accum_losses,
+                    'n_token': accum_n_tokens,
+                    'lr': lrs,
+                    'memory': mems
+                })
+                df.to_csv(f"{result_dir}/step_data/{rank}.csv")
+                df = pd.DataFrame({
+                    'data_time': data_times,
+                    'forward_time': loss_times,
+                    'batch_size': batch_sizes,
+                    'max_len': max_lens
+                })
+                df.to_csv(f"{result_dir}/step_data/{rank}_step.csv")
+                if is_main:
+                    torch.save(model.state_dict(), f"{result_dir}/models/{opt_step}.pth")
+                    cleardir(f"{result_dir}/optimizers")
+                    torch.save(optimizer.state_dict(), f"{result_dir}/optimizers/{opt_step}.pth")
+                if args.gc:
+                    gc.collect()
+            n_accum_token = 0
+            accum_loss = 0
+
+            if opt_step >= args.max_opt_step:
+                break
+        if (step+1) % log_step == 0:
+            logger.info(f"{step+1} step finished.")
+    logger.info("Training finished!")
