81c4d88ad3e57c67fe4ed48fba64fd293ae759e7
yyoshikai <yyoshikai12@gmail.com>
2025-06-19 13:33:35 +0900

    mamba

--- mamba_env.sh
+++
@@ -1,6 +0,0 @@
-deactivate
-source /workspace/envs/cu124/pyenv/3.11.9/.bashrc
-cd /workspace/envs/cu124/3.11.9
-rm -r mamba_test
-
-
--- mamba_train.sh
+++
@@ -1,8 +0,0 @@
-export CUBLAS_WORKSPACE_CONFIG=:4096:8
-
-torchrun --nproc-per-node 1 train.py --studyname test --token-per-step 160000 \
-    --mol-repeat 1 --pocket-repeat 5 --frag-repeat 0 --num-workers 28 \
-    --mol-data /workspace/ssd/cheminfodata/unimol/ligands/train.lmdb \
-    --pocket-data /workspace/ssd/cheminfodata/unimol/pockets/train.lmdb \
-    --test --sdp-kernel FLASH --mamba \
-    --pocket-coord-heavy --logtime --prefetch-factor 10 --coord-follow-atom
