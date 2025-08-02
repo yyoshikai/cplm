9971c136ade2505df386cf8c3199068a80021007
yyoshikai <yyoshikai12@gmail.com>
2025-08-02 22:04:51 +0900

    check_reinforce_generation

--- pocket_conditioned_generation/reinforce/test.sh
+++ pocket_conditioned_generation/reinforce/test.sh
@@ -1,9 +1,9 @@
-rm -r results/250530_test
+rm -r 250530_test
 
 python generate.py --data-dir /workspace/cplm/preprocess/results/finetune/r4_all --sname 250521_g16_min_5000_a0000005 --step 10000 --index 250530_pm --max-len 1500 --token-per-batch 150000 --genname 250530_test
 
 sha_base=`sha1sum 250530_test/250530_pm/250521_g16_min_5000_a0000005/10000/info.csv`
-sha=`sha1sum results/250530_base/250521_g16_min_5000_a0000005/10000/250530_pm/info.csv`
+sha=`sha1sum 250530_base/250521_g16_min_5000_a0000005/10000/250530_pm/info.csv`
 sha_base=${sha_base% *}
 sha=${sha% *}
 
