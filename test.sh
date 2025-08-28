668e9862becb508c686609ef84d6704491124650
yyoshikai <yyoshikai12@gmail.com>
2025-08-28 18:49:22 +0900

    check_train_pocket

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-python test_train_mol.py && python test_finetune_data.py && git add . && git commit -m check_train_finetune_data
+# python test_train_mol.py && python test_finetune_data.py && git add . && git commit -m check_train_finetune_data
 
-
+python test_train_pocket.py && git add . && git commit -m check_train_pocket
---
+++ test_train_pocket.py
@@ -0,0 +1,51 @@
+# ref
+import sys, os
+import yaml
+from addict import Dict
+sys.path.append("/workspace/cplm")
+from src.data.pretrain import UniMolPocketDataset, ProteinDataset
+from src.data.coord_transform import CoordTransform
+from src.data.tokenizer import ProteinAtomTokenizer, FloatTokenizer
+
+org_dir = "/workspace/cplm/training/results/250619_mamba"
+with open(f"{org_dir}/config.yaml") as f:
+    args = Dict(yaml.safe_load(f))
+args.mol_data = '/workspace/cheminfodata/unimol/ligands/train.lmdb'
+args.pocket_data = '/workspace/cheminfodata/unimol/pockets/train.lmdb'
+
+coord_transform = CoordTransform(args.seed, True, True, args.coord_noise_std)
+protein_atom_tokenizer = ProteinAtomTokenizer(log_interval=args.tokenizer_log_interval)
+coord_tokenizer = FloatTokenizer(-args.coord_range, args.coord_range, log_interval=args.tokenizer_log_interval)
+pocket_data = UniMolPocketDataset(args.pocket_data, idx_to_key='str')
+pocket_data = ProteinDataset(pocket_data, protein_atom_tokenizer, coord_tokenizer, coord_transform, atom_heavy=not args.no_pocket_atom_heavy, coord_heavy=args.pocket_coord_heavy, atom_h=args.pocket_atom_h, coord_h=args.pocket_coord_h, coord_follow_atom=args.coord_follow_atom)
+vocs = pocket_data.vocs()
+
+items = [pocket_data[i] for i in range(3)]
+
+# mod
+import sys, os
+import yaml
+from addict import Dict
+sys.path.append("/workspace/cplm")
+from src.data.pretrain import UniMolPocketDataset, ProteinDataset
+from src.data.coord_transform import CoordTransform
+from src.data.tokenizer import ProteinAtomTokenizer, FloatTokenizer
+
+org_dir = "/workspace/cplm/training/results/250619_mamba"
+with open(f"{org_dir}/config.yaml") as f:
+    args = Dict(yaml.safe_load(f))
+args.mol_data = '/workspace/cheminfodata/unimol/ligands/train.lmdb'
+args.pocket_data = '/workspace/cheminfodata/unimol/pockets/train.lmdb'
+
+coord_transform = CoordTransform(args.seed, True, True, args.coord_noise_std)
+protein_atom_tokenizer = ProteinAtomTokenizer(log_interval=args.tokenizer_log_interval)
+coord_tokenizer = FloatTokenizer(-args.coord_range, args.coord_range, log_interval=args.tokenizer_log_interval)
+pocket_data = UniMolPocketDataset(args.pocket_data, idx_to_key='str')
+pocket_data = ProteinDataset(pocket_data, protein_atom_tokenizer, coord_tokenizer, coord_transform, atom_heavy=not args.no_pocket_atom_heavy, coord_heavy=args.pocket_coord_heavy, atom_h=args.pocket_atom_h, coord_h=args.pocket_coord_h, coord_follow_atom=args.coord_follow_atom)
+vocs0 = pocket_data.vocs()
+
+items0 = [pocket_data[i] for i in range(3)]
+
+# check
+assert vocs == vocs0
+assert all(item == item0 for item, item0 in zip(items, items0))
