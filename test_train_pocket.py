3cbfe358f8e405a0e504b7406225ac940d443d72
yyoshikai <yyoshikai12@gmail.com>
2025-08-28 19:56:14 +0900

    check_train_pocket_coord_follow

--- test_train_pocket.py
+++ test_train_pocket.py
@@ -29,7 +29,8 @@ for coord_follow in [False, True]:
     import numpy as np
     from addict import Dict
     sys.path.append("/workspace/cplm")
-    from src.data.pretrain2 import UniMolPocketDataset, ProteinProcessDataset, CoordFollowDataset
+    from src.data.pretrain2 import UniMolPocketDataset, CoordFollowDataset
+    from src.data.finetune2 import ProteinProcessDataset
     from src.data.coord_transform2 import CoordTransformDataset
     from src.data.tokenizer import ProteinAtomTokenizer, FloatTokenizer, TokenizeDataset, ArrayTokenizeDataset, SentenceDataset
     from src.data import untuple
@@ -40,16 +41,15 @@ for coord_follow in [False, True]:
     args.mol_data = '/workspace/cheminfodata/unimol/ligands/train.lmdb'
     args.pocket_data = '/workspace/cheminfodata/unimol/pockets/train.lmdb'
 
-#    coord_transform = CoordTransform(args.seed, True, True, args.coord_noise_std)
     protein_atom_tokenizer = ProteinAtomTokenizer(log_interval=args.tokenizer_log_interval)
     coord_tokenizer = FloatTokenizer(-args.coord_range, args.coord_range, log_interval=args.tokenizer_log_interval)
     pocket_data = UniMolPocketDataset(args.pocket_data, idx_to_key='str')
-    pocket_data = ProteinProcessDataset(pocket_data, atom_heavy=not args.no_pocket_atom_heavy, coord_heavy=args.pocket_coord_heavy, atom_h=args.pocket_atom_h, coord_h=args.pocket_coord_h)
+    pocket_data = ProteinProcessDataset(pocket_data, heavy_atom=not args.no_pocket_atom_heavy, heavy_coord=args.pocket_coord_heavy, h_atom=args.pocket_atom_h, h_coord=args.pocket_coord_h)
 
     atoms, coord, coord_position = untuple(pocket_data, 3)
+
     coord = CoordTransformDataset(coord, rstate=np.random.default_rng(args.seed), normalize_coord=True, random_rotate=True, coord_noise_std=args.coord_noise_std)
     coord = untuple(coord, 1)[0]
-
     atoms = TokenizeDataset(atoms, protein_atom_tokenizer)
     coord = ArrayTokenizeDataset(coord, coord_tokenizer)
     if coord_follow:
