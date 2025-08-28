8914760f528279107e45f2f0ff7f31793be37721
yyoshikai <yyoshikai12@gmail.com>
2025-08-28 19:15:16 +0900

    check_train_pocket_coord_follow

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # python test_train_mol.py && python test_finetune_data.py && git add . && git commit -m check_train_finetune_data
 
-python test_train_pocket.py && python test_train_pocket_coord_follow.py && git add . && git commit -m check_train_pocket
+python test_train_pocket.py && python test_train_pocket_coord_follow.py && git add . && git commit -m check_train_pocket_coord_follow
