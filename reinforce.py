449a97d2e85f7a36afa14b3e0a488133f1552fec
yyoshikai <yyoshikai12@gmail.com>
2025-10-18 19:53:33 +0900

    checkpoint_for_training

--- reinforce.py
+++ reinforce.py
@@ -1,5 +1,4 @@
 import sys, os, yaml, shutil, psutil, gc, math, random, re
-print(os.environ, flush=True)
 import itertools as itr
 import concurrent.futures as cf
 from argparse import ArgumentParser
