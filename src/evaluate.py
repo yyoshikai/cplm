88b3cede5ef2f47fd2a5ef4e4c6a5686ea7a9bc7
yyoshikai <yyoshikai12@gmail.com>
2025-10-22 15:16:55 +0900

    fix

--- src/evaluate.py
+++ src/evaluate.py
@@ -548,8 +548,7 @@ def eval_qvina3(lig_path, rec_path, out_dir, lig_idx=0, use_uff=True, center=Non
         )
         
         path_to_qvina = os.environ.get('QVINA_PATH', f"{WORKDIR}/github/qvina/bin/qvina02")
-        commands = f"""cd {out_dir} && {path_to_qvina} --receptor rec.pdbqt --ligand lig.pdbqt --center_x {center[0]:.4f} --center_y {center[1]:.4f} --center_z {center[2]:.4f} --size_x 20 --size_y 20 --size_z 20 --exhaustiveness {exhaustiveness}
-        """
+        commands = f"cd {out_dir} && {path_to_qvina} --receptor rec.pdbqt --ligand lig.pdbqt --center_x {center[0]:.4f} --center_y {center[1]:.4f} --center_z {center[2]:.4f} --size_x 20 --size_y 20 --size_z 20 --exhaustiveness {exhaustiveness}"
         if cpu is not None:
             commands += f" --cpu {cpu}"
         proc.stdin.write(commands.encode('utf-8'))
