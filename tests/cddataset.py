83d2638d987b2760d914d7120a12ed2c52987f0d
yyoshikai <yyoshikai12@gmail.com>
2025-10-28 22:57:24 +0900

    no_test

--- tests/cddataset.py
+++ tests/cddataset.py
@@ -9,21 +9,9 @@ from src.utils.logger import get_logger
 
 logger = get_logger(stream=True)
 dlargs = dict(batch_size=None, shuffle=False, num_workers=16)
-# indexが重複していないか？
-datas = {}
-idxs = {}
-for split in ['valid', 'test', 'train']:
-    datas[split] = CDDataset(split)
-    logger.info(f"{split=}, {len(datas[split])=}")
-    idxs[split] = list(DataLoader(datas[split].indices, **dlargs))
-
-for split0, split1 in itr.combinations(['train', 'valid', 'test'], 2):
-    idx0 = idxs[split0]
-    idx1 = idxs[split1]
-    logger.info(f"{split0=}, {split1=}, {len(idx0)=}, {len(idx1)=}, {len(idx0&idx1)=}, {len(idx0)|len(idx1)=}")
 
 # test setのdnameがsplit_by_nameと一致しているか？
-loader = DataLoader(datas['test'], **dlargs)
+loader = DataLoader(CDDataset('test'), **dlargs)
 test_dnames = {item[3].split('/')[-2] for item in loader}
 split_by_name = torch.load("/workspace/cheminfodata/crossdocked/targetdiff/split_by_name.pt", weights_only=True)
 test_dnames0 = {item[0].split('/')[0] for item in split_by_name['test']}
