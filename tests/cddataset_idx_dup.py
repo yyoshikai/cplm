899613b8085191832b521cdae679e0bd0802ada7
yyoshikai <yyoshikai12@gmail.com>
2025-10-29 23:04:05 +0900

    test_idx_dup

--- tests/cddataset_idx_dup.py
+++ tests/cddataset_idx_dup.py
@@ -6,15 +6,17 @@ from torch.utils.data import DataLoader
 WORKDIR = os.environ.get('WORKDIR', "/workspace")
 sys.path.append(f"{WORKDIR}/cplm")
 from src.data.datasets.crossdocked import CDDataset
+from src.data import ApplyDataset
 from src.utils.logger import get_logger
 
 logger = get_logger(stream=True)
 dlargs = dict(batch_size=None, shuffle=False, num_workers=16)
 
 # test setのdnameがsplit_by_nameと一致しているか？
-loader = DataLoader(CDDataset('test'), **dlargs)
-test_dnames = {item[3].split('/')[-2] for item in tqdm(loader)}
-split_by_name = torch.load("/workspace/cheminfodata/crossdocked/targetdiff/split_by_name.pt", weights_only=True)
-test_dnames0 = {item[0].split('/')[0] for item in split_by_name['test']}
-logger.info(f"{len(test_dnames)=}, {len(test_dnames0)=}, {len(test_dnames0&test_dnames)=}")
+idxs = {}
+for split in ['valid', 'test', 'train']:
+    indices = CDDataset(split).indices
+    idxs[split] = set(tqdm(DataLoader(indices, **dlargs)))
 
+for split0, split1 in itr.combinations(['train', 'valid', 'test'], 2):
+    print(f"{split0=}, {split1=}, {len(idxs[split0])=}, {len(idxs[split1])=}, {len(idxs[split0]&idxs[split1])=}, ")
