310ce6da966633a2db2d260dff96e07718ca1973
yyoshikai <yyoshikai12@gmail.com>
2025-10-29 23:10:37 +0900

    fix_idx_dup

--- tests/cddataset_idx_dup.py
+++ tests/cddataset_idx_dup.py
@@ -12,7 +12,7 @@ dlargs = dict(batch_size=None, shuffle=False, num_workers=16)
 idxs = {}
 for split in ['valid', 'test', 'train']:
     env, txn = load_lmdb(f"{WORKDIR}/cheminfodata/crossdocked/pockets/mask/{split}_idxs.lmdb", readahead=True)
-    idxs[split] = {int.from_bytes(key) for key in txn.cursor().iternext(values=False)}
+    idxs[split] = {int.from_bytes(value) for value in txn.cursor().iternext(keys=False)}
 
 for split0, split1 in itr.combinations(['train', 'valid', 'test'], 2):
     print(f"{split0=}, {split1=}, {len(idxs[split0])=}, {len(idxs[split1])=}, {len(idxs[split0]&idxs[split1])=}")
