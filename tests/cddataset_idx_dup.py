565a9fcf96b6c647f58ff7b03e293769a9d94341
yyoshikai <yyoshikai12@gmail.com>
2025-10-29 23:08:47 +0900

    fix_idx_dup

--- tests/cddataset_idx_dup.py
+++ tests/cddataset_idx_dup.py
@@ -5,18 +5,14 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 WORKDIR = os.environ.get('WORKDIR', "/workspace")
 sys.path.append(f"{WORKDIR}/cplm")
-from src.data.datasets.crossdocked import CDDataset
-from src.data import ApplyDataset
-from src.utils.logger import get_logger
-
-logger = get_logger(stream=True)
+from src.utils.lmdb import load_lmdb
 dlargs = dict(batch_size=None, shuffle=False, num_workers=16)
 
-# test setのdnameがsplit_by_nameと一致しているか？
+# idxが重複していないか そのままだと時間がかかるのでlmdbを直接調べる
 idxs = {}
 for split in ['valid', 'test', 'train']:
-    indices = CDDataset(split).indices
-    idxs[split] = set(tqdm(DataLoader(indices, **dlargs)))
+    env, txn = load_lmdb(f"{WORKDIR}/cheminfodata/crossdocked/pockets/mask/{split}_idxs.lmdb", readahead=True)
+    idxs[split] = {int.from_bytes(key) for key in txn.cursor().iternext(values=False)}
 
 for split0, split1 in itr.combinations(['train', 'valid', 'test'], 2):
-    print(f"{split0=}, {split1=}, {len(idxs[split0])=}, {len(idxs[split1])=}, {len(idxs[split0]&idxs[split1])=}, ")
+    print(f"{split0=}, {split1=}, {len(idxs[split0])=}, {len(idxs[split1])=}, {len(idxs[split0]&idxs[split1])=}")
