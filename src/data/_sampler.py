ca3b5dbef3b54d822d1d787bc4b3e59813cd82ba
yyoshikai <yyoshikai12@gmail.com>
2026-06-29 14:23:01 +0900

    sampler

--- src/data/_sampler.py
+++ src/data/_sampler.py
@@ -7,19 +7,23 @@ from torch.utils.data import Sampler
 
 class InfiniteRandomSampler(Sampler):
     logger = getLogger(f"{__module__}.{__qualname__}")
-    def __init__(self, dataset: Sized, generator: torch.Generator=None):
-        self.dataset = dataset
+    def __init__(self, dataset: Sized, generator: torch.Generator=None, mmap_path: str|None=None):
+        self.size = len(dataset)
+        self.mmap_path = mmap_path
         self.generator = generator
         self.epoch = None
+        self._order = None
 
     def __iter__(self) -> Iterator[int]:
         self.epoch = 0
         while True:
             self.logger.info('Calculating sample order...')
-            order = torch.randperm(len(self.dataset), generator=self.generator).tolist()
+            order = torch.randperm(self.size, generator=self.generator).numpy()
             self.logger.info('calculated.')
-            # self.logger.warning("Skipping to 72 for debug!!!")
-            yield from order
-            # yield from order[::72]
+            if self.mmap_path is None:
+                self._order = order
+            else:
+                self._order = np.memmap(self.mmap_path, dtype=np.int64, mode='w+', shape=(self.size,))
+            yield from map(int, self._order)
             self.epoch += 1
             self.logger.info(f"{self.epoch} epoch finished.")
